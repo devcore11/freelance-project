be5cfb434abc5e9382b7a8195185a9def746e26e
prais-franks <frankspraise2@gmail.com>
2025-01-01 01:44:36 +0100

    freelance website

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-
-const sidebarBtn = document.querySelector(".sidebar-toggle-btn");
+const sidebarBtn = document.querySelector(".toggle-btn");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 const windows = document.querySelector("body");
 const sidebarShadow = document.querySelector(".shadow-overlay");
 
-function closeSidebar() {
-  sidebar.classList.remove("show-sidebar");
-  windows.style.overflowY = "scroll";
-  sidebarShadow.classList.remove("show-shadow");
-}
-
-function sidebarShadowToggle() {
-  if (sidebar.classList.contains("show-sidebar")) {
-    sidebarShadow.classList.add("show-shadow");
-  } else {
-    sidebarShadow.classList.remove("show-shadow");
-  }
-}
-
 sidebarBtn.addEventListener("click", function (event) {
   event.stopPropagation();
+
   sidebar.classList.add("show-sidebar");
 
   windows.style.overflowY = "hidden";
-  window.addEventListener("click", closeSidebar);
 
-  sidebarShadow.classList.toggle("show-shadow")
+  window.addEventListener("click", closeSidebar);
+  sidebarShadow.classList.toggle("show-shadow");
   sidebarShadowToggle();
 });
+
+function closeSidebar() {
+  sidebar.classList.remove("show-sidebar");
+  sidebarShadow.classList.remove("show-shadow");
+  windows.style.overflowY = "scroll";
+}
 
 closeBtn.addEventListener("click", function (event) {
   closeSidebar();
@@ -38,16 +30,16 @@ sidebar.addEventListener("click", function (event) {
   event.stopPropagation();
 });
 
-
 const navbarContainer = document.querySelector(".navbar-container");
 
-window.addEventListener("scroll", function () {
-  const scrollHeight = window.pageYOffset;
-  const navHeight = navbarContainer.getBoundingClientRect().height;
 
-  if (scrollHeight > navHeight) {
-    navbarContainer.classList.add("fixed-navbar");
-  } else {
-    navbarContainer.classList.remove("fixed-navbar");
-  }
-});
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbarContainer.getBoundingClientRect().height
+   
+    if(scrollHeight > navHeight){
+       navbarContainer.classList.add("fixed-navbar")
+    } else{
+       navbarContainer.classList.remove("fixed-navbar")
+    }
+})
